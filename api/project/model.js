a4b4242288632b74e649b99ac5b73cd340811fe5
Lance Turbes Jr <waywardpooch@users.noreply.github.com>
2021-11-05 07:39:55 -0700

    updated project model with 3 helpers (untested)

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,27 @@
 // build your `Project` model here
+const db = require("./../../data/dbConfig");
+
+const getProjects = async () => {
+  const projects = await db("projects");
+  return projects;
+};
+
+const getProjectById = async (project_id) => {
+  const project = await db("projects")
+    .where("project_id", project_id)
+    .first();
+  return project;
+};
+
+const addProject = async (project) => {
+  const [id] = await db("projects")
+    .insert(project);
+  const newProject = await getProjectById(id);
+  return newProject;
+};
+
+module.exports = {
+  getProjects,
+  getProjectById,
+  addProject,
+};
